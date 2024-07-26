cdab85fd5e4cfa080250cad3482cfde3005f05e7
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-26 02:49:25 -0700

    new change has been made

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 CREATE TABLE BookS(book_id INT PRIMARY KEY,
 title VARCHAR(130),
-author_id (FOREIGN KEY REFERENCING Authors TABLE),
+author_id (FOREIGN KEY REFERENCES Authors TABLE),
 price DOUBLE,
 publication_date DATE)
 CREATE TABLE Authors(author_id (PRIMARY KEY),
@@ -11,9 +11,9 @@ CREATE TABLE Customers(customer_id (PRIMARY KEY),
 customer_name VARCHAR(215),
 email VARCHAR(215),address TEXT)
 CREATE TABLE Orders (order_id (PRIMARY KEY),
-customer_id (FOREIGN KEY REFERENCING Customers TABLE),
+customer_id (FOREIGN KEY REFERENCES Customers TABLE),
 order_date DATE)
 CREATE TABLE Order_Details(orderdetailid (PRIMARY KEY),
-order_id (FOREIGN KEY REFERENCING Orders TABLE),
-book_id (FOREIGN KEY REFERENCING Books TABLE),
+order_id (FOREIGN KEY REFERENCES Orders TABLE),
+book_id (FOREIGN KEY REFERENCES Books TABLE),
 quantity DOUBLE)
