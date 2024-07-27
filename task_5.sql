3ffa3bfeaa67f8000ce4930f1634501bcf0f5b4c
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-27 01:29:54 -0700

    new file is added

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+INSERT INTO Customers(customer_id,customer_name,email,address) VALUES(customer_id = 1,
+customer_name = 'Cole Baidoo',
+email ='cbaidoo@sandtech.com ',
+address ='123 Happiness Ave')
