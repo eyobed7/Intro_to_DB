92f2832c4a0d63b305536ebf9721b9aabcd98018
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-27 01:34:14 -0700

    added

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,2 @@
-USE alx_book_store;
-INSERT INTO Customers(customer_id,customer_name,email,address) VALUES(customer_id = 1,
-customer_name = 'Cole Baidoo',
-email ='cbaidoo@sandtech.com ',
-address ='123 Happiness Ave')
+INSERT INTO Customers (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
